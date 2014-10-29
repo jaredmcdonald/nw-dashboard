05b9a5db65f359f5c29ed94961aabc6af072aa58
Jared McDonald <jared.mcdonald@jcrew.com>
2014-10-29 14:11:35 -0400

    poll for weather every 5 mins

--- modules/weather/index.js
+++ modules/weather/index.js
@@ -1,5 +1,7 @@
 var request = require('request')
 ,   getView = require('../../view-getter')
+,   pollFrequency = 60 * 5 * 1000 // 5 mins
+,   intervalKey = null
 
 function handleWeatherResponse (template, body) {
   var data = {
@@ -11,18 +13,31 @@ function handleWeatherResponse (template, body) {
   window.document.getElementById('weather').innerHTML = template(data)
 }
 
+function getWeather (key, template) {
+    console.log('getWeather. . . . ')
+    request('http://api.wunderground.com/api/' + key + '/conditions/q/autoip.json', function (err, res, body) {
+    if (err) {
+      console.error(err)
+      return false
+    }
+    handleWeatherResponse(template, JSON.parse(body))
+  })
+}
+
+
 module.exports = function (key) {
   getView('weather', function (err, template) {
     if (err) {
       console.error(err)
       return false
     }
-    request('http://api.wunderground.com/api/' + key + '/conditions/q/autoip.json', function (apiErr, res, body) {
-      if (apiErr) {
-        console.error(apiErr)
-        return false
-      }
-      handleWeatherResponse(template, JSON.parse(body))
-    })
+    getWeather(key, template)
+    intervalKey = setInterval(getWeather, pollFrequency, key, template)
   })
+
+  return {
+    stop : function () {
+      clearInterval(intervalKey)
+    }
+  }
 }
